448bbd40bfbda0beb9879393a5bcf3b3c8b2a1cc
Gagan Singh <gagansingh.dev@outlook.com>
2021-02-16 23:06:15 -0500

    Implemented basic CRUD infrastructure for campground

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const Campground = require("./models/campground");
 // INIT THE APP
 const app = express();
 
+// MIDDLEWARES
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 // MAKE MONGODB TO APP CONNECTION
 mongoose
   .connect("mongodb://localhost:27017/happy-camper", {
@@ -30,15 +34,52 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-// test
-app.get("/makeCampground", async (req, res) => {
-  const camp = new Campground({
-    title: "Algonquin Park",
-    description:
-      "A huge conservation site with numerous campgrounds open to the public.",
+// Campgrounds index
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+// Add a new campground -> SERVE THE FORM
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+// Add a new campground -> POST request from the form
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// Find campground by id
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/show", { campground });
+});
+
+// EDIT a campground -> SERVE THE FORM
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+
+// EDIT a campground -> PUT request from the form
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
   });
-  await camp.save();
-  res.json(camp);
+  res.redirect(`/campgrounds/${id}`);
+});
+
+// DELETE route
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
 });
 
 // SERVE THE APP
