a84d47d64aee7b68d733130933a67084bd4318b5
Gagan Singh <gagansingh.dev@outlook.com>
2021-02-22 17:51:25 -0500

    Implemented review deletion middleware upon deletion of a campground

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const Review = require("./review");
+
 const CampgroundSchema = new Schema({
   title: String,
   image: String,
@@ -15,6 +17,23 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+// MIDDLEWARES
+// Triggered when findByIdAndDelete is called and successful
+// https://mongoosejs.com/docs/api.html#model_Model.findByIdAndDelete
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  // What is doc? after running findOneAndDelete, the document that is deleted is
+  // passed onto this post middleware
+  if (doc) {
+    // Delete all reviews whose IDs are present in the campground.reviews
+    // document that was just deleted
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
