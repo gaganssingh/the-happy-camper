ab42f9728e57f4dc274e2f9d5525fc1fbcba72d7
Gagan Singh <gagansingh.dev@outlook.com>
2021-02-25 23:56:26 -0500

    renamed filename for clarity

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,35 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 // IMPORTED CONTROLLERS
-const campgrounds = require("../controllers/campgrounds");
+const campgroundsController = require("../controllers/campgroundsController");
 
 // CUSTOM IMPORTS
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
 // GET -> All Campgrounds
-router.get("/", catchAsync(campgrounds.index));
+router.get("/", catchAsync(campgroundsController.index));
 
 // GET -> Serve the new campground form
-router.get("/new", isLoggedIn, campgrounds.renderNewCampgroundForm);
+router.get("/new", isLoggedIn, campgroundsController.renderNewCampgroundForm);
 
 // POST -> Create a new campground
 // prettier-ignore
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.post("/", isLoggedIn, validateCampground, catchAsync(campgroundsController.createCampground));
 
 // GET -> Find campground by id
-router.get("/:id", catchAsync(campgrounds.findAndShowCampground));
+router.get("/:id", catchAsync(campgroundsController.findAndShowCampground));
 
 // GET -> Serve the EDIT campground form
 // prettier-ignore
-router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditCampgroundForm));
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgroundsController.renderEditCampgroundForm));
 
 // PUT -> EDIT a campground logic
 // prettier-ignore
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground));
+router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundsController.editCampground));
 
 // DELETE route
 // prettier-ignore
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgroundsController.deleteCampground));
 
 module.exports = router;
