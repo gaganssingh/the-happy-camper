514952539763b80b87d2a7ea3d2c822e09f13767
Gagan Singh <gagansingh.dev@outlook.com>
2021-02-26 15:40:56 -0500

    Separated Image into its own schema to use virtuals to generate thumbnail url

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,19 @@ const Schema = mongoose.Schema;
 
 const Review = require("./review");
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+// GENERATE THUMBNAIL IMAGE FROM THE CLOUDINARY IMAGE URL
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w-200");
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
